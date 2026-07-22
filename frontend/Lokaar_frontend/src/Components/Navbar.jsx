0d91ca80f16924ece05a237ce2b3fa5fc73cdf82
QaziAbdulGhafoor <qzgh986@gmail.com>
2026-07-22 17:36:22 +0500

    nav and footer completed

--- frontend/Lokaar_frontend/src/Components/Navbar.jsx
+++ frontend/Lokaar_frontend/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import "./Navbar.css";
-import "../App.css"
+import "../App.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -9,16 +9,24 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <li>
-          <img src={logo} alt="" className="logo"/>
+          <img src={logo} alt="" className="logo ml-6" />
         </li>
       </div>
       <div className="services">
-        <li><a href="">Services</a></li>
-        <li><a href="">How It Works</a></li>
-        <li><a href="">Become A Provider</a></li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">How It Works</a>
+        </li>
+        <li>
+          <a href="">Become A Provider</a>
+        </li>
       </div>
       <div className="registers">
-        <li><a href="">Log In</a></li>
+        <li>
+          <a href="">Log In</a>
+        </li>
         <li>
           <a href="/login">
             <button>Sign Up</button>
