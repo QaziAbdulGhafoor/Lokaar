b5258ad6763f8d282c0007c701cb119065e918df
QaziAbdulGhafoor <qzgh986@gmail.com>
2026-08-25 15:11:03 +0500

    working on all chats page

--- backend/routes/conversation.js
+++ backend/routes/conversation.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const middlewares = require("../middleware/middleware");
 const Conversation = require("../models/Conversation");
+const Message = require("../models/Message");
 
 router.get("/", middlewares.isLoggedIn, async (req, res) => {
   const conversations = await Conversation.find({
@@ -9,6 +10,12 @@ router.get("/", middlewares.isLoggedIn, async (req, res) => {
   }).populate("participants");
   console.log(conversations);
   res.json({ conversations });
+});
+
+router.get("/:id", middlewares.isLoggedIn, async (req, res) => {
+  const { id } = req.params;
+  const messages = await Message.find({ conversation: id });
+  res.json({ messages });
 });
 
 module.exports = router;
